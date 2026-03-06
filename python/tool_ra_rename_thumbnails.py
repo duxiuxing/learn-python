8092db2e66cbe6c753f5ed014e19a08ef70b6819
duxiuxing <duxiuxing@foxmail.com>
2026-03-07 07:54:54 +0800

    add common .py

---
+++ python/tool_ra_rename_thumbnails.py
@@ -0,0 +1,214 @@
+# -- coding: UTF-8 --
+
+import json
+import os
+
+from game import Game
+from games_db import GamesDB
+from init_global_configs import Init_Global_Configs
+from local_configs import LocalConfigs
+from pathlib import Path
+from rom import Rom
+from roms_db import RomsDB
+from wii_ra_configs import WiiRA_Configs
+from wiiflow_rom import WiiFlow_Rom
+from wiiflow_roms_db import WiiFlow_RomsDB
+
+
+class RA_RenameThumbnails:
+    @staticmethod
+    def default_lpl_file_path():
+        return LocalConfigs.retroarch_directory().joinpath(
+            "playlists", WiiRA_Configs.db_name()
+        )
+
+    @staticmethod
+    def default_thumbnails_directory():
+        return LocalConfigs.retroarch_directory().joinpath(
+            "thumbnails", WiiRA_Configs.db_name().stem
+        )
+
+    @staticmethod
+    def f3_rom_file_title_to_game_en_title(lpl_file_path: Path, png_folder_path: Path):
+        # 3wonders.png -> Three Wonders.png
+        if not png_folder_path.exists() or not png_folder_path.is_dir():
+            print(f"【错误】无效的文件夹 {png_folder_path}")
+            return
+
+        with open(lpl_file_path, "r", encoding="utf-8") as file:
+            items = json.load(file)["items"]
+            for item in items:
+                rom_file_name = Path(item["path"]).name
+                rom_file_title = Path(rom_file_name).stem
+                rom = RomsDB.query_rom(rom_file_name=rom_file_name)
+                if rom is None:
+                    rom = WiiFlow_RomsDB.query_rom(rom_file_title=rom_file_title)
+                game = GamesDB.query_game(game_id=rom.game_id)
+
+                old_file_path = png_folder_path.joinpath(f"{rom_file_title}.png")
+                new_file_path = png_folder_path.joinpath(f"{game.en_title}.png")
+                if old_file_path.exists() and old_file_path.is_file():
+                    os.rename(old_file_path, new_file_path)
+                elif not new_file_path.exists():
+                    print(f"【错误】无效的源文件 {old_file_path}")
+
+    @staticmethod
+    def f4_game_en_title_to_rom_file_title(lpl_file_path: Path, png_folder_path: Path):
+        # Three Wonders.png -> 3wonders.png
+        if not png_folder_path.exists() or not png_folder_path.is_dir():
+            print(f"【错误】无效的文件夹 {png_folder_path}")
+            return
+
+        with open(lpl_file_path, "r", encoding="utf-8") as file:
+            items = json.load(file)["items"]
+            for item in items:
+                rom_file_name = Path(item["path"]).name
+                rom_file_title = Path(rom_file_name).stem
+                rom = RomsDB.query_rom(rom_file_name=rom_file_name)
+                if rom is None:
+                    rom = WiiFlow_RomsDB.query_rom(
+                        rom_file_title=Path(rom_file_name).stem
+                    )
+                game = GamesDB.query_game(game_id=rom.game_id)
+
+                old_file_path = png_folder_path.joinpath(f"{game.en_title}.png")
+                new_file_path = png_folder_path.joinpath(f"{rom_file_title}.png")
+                if old_file_path.exists() and old_file_path.is_file():
+                    os.rename(old_file_path, new_file_path)
+                elif not new_file_path.exists():
+                    print(f"【错误】无效的源文件 {old_file_path}")
+
+    @staticmethod
+    def f2_rom_file_title_to_label(lpl_file_path: Path, png_folder_path: Path):
+        # 3wonders.png -> label
+        if not png_folder_path.exists() or not png_folder_path.is_dir():
+            print(f"【错误】无效的文件夹 {png_folder_path}")
+            return
+
+        with open(lpl_file_path, "r", encoding="utf-8") as file:
+            items = json.load(file)["items"]
+            for item in items:
+                rom_file_title = Path(item["path"]).stem
+                original_label = item["label"]
+                label = original_label.replace(":", "_").replace("/", "_")
+
+                old_file_path = png_folder_path.joinpath(f"{rom_file_title}.png")
+                new_file_path = folder_path.joinpath(f"{label}.png")
+                if old_file_path.exists() and old_file_path.is_file():
+                    os.rename(old_file_path, new_file_path)
+                elif not new_file_path.exists():
+                    print(f"【错误】无效的源文件 {old_file_path}")
+
+    @staticmethod
+    def f1_label_to_rom_file_title(lpl_file_path: Path, png_folder_path: Path):
+        # label -> 3wonders.png
+        if not png_folder_path.exists() or not png_folder_path.is_dir():
+            print(f"【错误】无效的文件夹 {png_folder_path}")
+            return
+
+        with open(lpl_file_path, "r", encoding="utf-8") as file:
+            items = json.load(file)["items"]
+            for item in items:
+                rom_file_title = Path(item["path"]).stem
+                original_label = item["label"]
+                label = original_label.replace(":", "_").replace("/", "_")
+
+                old_file_path = png_folder_path.joinpath(f"{label}.png")
+                new_file_path = png_folder_path.joinpath(f"{rom_file_title}.png")
+                if old_file_path.exists() and old_file_path.is_file():
+                    os.rename(old_file_path, new_file_path)
+                elif not new_file_path.exists():
+                    print(f"【错误】无效的源文件 {old_file_path}")
+
+    @staticmethod
+    def f5_label_to_game_en_title(lpl_file_path: Path, png_folder_path: Path):
+        RA_RenameThumbnails.f1_label_to_rom_file_title(
+            lpl_file_path=lpl_file_path, png_folder_path=png_folder_path
+        )
+        RA_RenameThumbnails.f3_rom_file_title_to_game_en_title(
+            lpl_file_path=lpl_file_path, png_folder_path=png_folder_path
+        )
+
+
+if __name__ == "__main__":
+    Init_Global_Configs()
+
+    lpl_file_path = None
+    while True:
+        lpl_file_path = RA_RenameThumbnails.default_lpl_file_path()
+        print("\n即将根据 .lpl 文件对缩略图文件进行重命名")
+        print(f"\n默认 .lpl 文件路径：{lpl_file_path}")
+        user_input = input("请输入 .lpl 文件路径，使用默认路径请直接按回车 > ")
+        if len(user_input) > 0:
+            lpl_file_path = Path(user_input)
+
+        if lpl_file_path.exists() and lpl_file_path.is_file():
+            break
+        else:
+            print(f"【错误】无效的文件路径：{lpl_file_path}")
+
+    thumbnails_dir = RA_RenameThumbnails.default_thumbnails_directory()
+    png_folder_path = None
+    while True:
+        print("\n以下是预设的文件夹路径：")
+        print(f"1.{thumbnails_dir.joinpath('Named_Boxarts')}")
+        print(f"2.{thumbnails_dir.joinpath('Named_Logos')}")
+        print(f"3.{thumbnails_dir.joinpath('Named_Snaps')}")
+        print(f"4.{thumbnails_dir.joinpath('Named_Titles')}")
+        user_input = input(
+            "请输入预设文件夹的序号或者自定义的文件夹路径（退出请直接按回车）\n> "
+        )
+        try:
+            number = int(user_input)
+            if number == 1:
+                png_folder_path = thumbnails_dir.joinpath("Named_Boxarts")
+            elif number == 2:
+                png_folder_path = thumbnails_dir.joinpath("Named_Boxarts")
+            elif number == 3:
+                png_folder_path = thumbnails_dir.joinpath("Named_Boxarts")
+            elif number == 4:
+                png_folder_path = thumbnails_dir.joinpath("Named_Boxarts")
+            else:
+                continue
+        except ValueError:
+            if len(user_input) > 0:
+                folder_path = Path(user_input)
+                if not folder_path.exists() or not folder_path.is_dir():
+                    print(f"【错误】无效的文件夹路径：{folder_path}")
+                    continue
+                else:
+                    png_folder_path = folder_path
+            else:
+                exit()
+
+        print("\n1. label -> 3wonders.png")
+        print("2. 3wonders.png -> label")
+        print("3. 3wonders.png -> Three Wonders.png")
+        print("4. Three Wonders.png -> 3wonders.png")
+        print("5. label -> Three Wonders.png")
+        print("其他输入表示重新设置文件夹路径")
+        user_input = input("请输入操作的序号 > ")
+        try:
+            number = int(user_input)
+            if number == 1:
+                RA_RenameThumbnails.f1_label_to_rom_file_title(
+                    lpl_file_path, png_folder_path
+                )
+            elif number == 2:
+                RA_RenameThumbnails.f2_rom_file_title_to_label(
+                    lpl_file_path, png_folder_path
+                )
+            elif number == 3:
+                RA_RenameThumbnails.f3_rom_file_title_to_game_en_title(
+                    lpl_file_path, png_folder_path
+                )
+            elif number == 4:
+                RA_RenameThumbnails.f4_game_en_title_to_rom_file_title(
+                    lpl_file_path, png_folder_path
+                )
+            elif number == 5:
+                RA_RenameThumbnails.f5_label_to_game_en_title(
+                    lpl_file_path, png_folder_path
+                )
+        except ValueError:
+            continue
---
+++ python/tool_ra_resize_logos.py
@@ -0,0 +1,126 @@
+# -- coding: UTF-8 --
+
+import fnmatch
+import os
+
+from helper import Helper
+from init_global_configs import Init_Global_Configs
+from local_configs import LocalConfigs
+from pathlib import Path
+from PIL import Image
+
+
+DESTINATION_LOGO_WIDTH = 400
+DESTINATION_LOGO_HEIGHT = 200
+
+
+def check_logo_left(logo, pixel_test):
+    for x in range(logo.width):
+        for y in range(logo.height):
+            if logo.getpixel((x, y)) != pixel_test:
+                return x
+    return 0
+
+
+def check_logo_top(logo, pixel_test):
+    for y in range(logo.height):
+        for x in range(logo.width):
+            if logo.getpixel((x, y)) != pixel_test:
+                return y
+    return 0
+
+
+def check_logo_right(logo, pixel_test):
+    for x_offset in range(1, logo.width + 1):
+        for y in range(logo.height):
+            if logo.getpixel((logo.width - x_offset, y)) != pixel_test:
+                return logo.width - x_offset
+    return logo.width - 1
+
+
+def check_logo_bottom(logo, pixel_test):
+    for y_offset in range(1, logo.height + 1):
+        for x in range(logo.width):
+            if logo.getpixel((x, logo.height - y_offset)) != pixel_test:
+                return logo.height - y_offset
+    return logo.height - 1
+
+
+def crop_logo(png_path):
+    logo = Image.open(png_path)
+    pixel_test = logo.getpixel((0, 0))
+
+    left = check_logo_left(logo, pixel_test)
+    top = check_logo_top(logo, pixel_test)
+    right = check_logo_right(logo, pixel_test)
+    bottom = check_logo_bottom(logo, pixel_test)
+
+    if left == 0 and top == 0 and right == logo.width - 1 and bottom == logo.height - 1:
+        return logo
+
+    return logo.crop((left, top, right + 1, bottom + 1))
+
+
+if __name__ == "__main__":
+    Init_Global_Configs()
+
+    src_dir = None
+    while True:
+        src_dir = LocalConfigs.repository_directory().joinpath("media\\logo-hd")
+        print("\n即将对源文件夹里的 Logo 文件进行重新剪裁")
+        print(f"\n默认源文件夹路径：{src_dir}")
+        user_input = input("请确认源文件夹路径，使用默认路径请直接按回车 > ")
+        if len(user_input) > 0:
+            src_dir = Path(user_input)
+
+        if src_dir.exists() and src_dir.is_dir():
+            break
+        else:
+            print(f"【错误】无效的源文件夹路径：{src_dir}")
+            continue
+
+    dst_dir = src_dir.joinpath(f"{DESTINATION_LOGO_WIDTH}x{DESTINATION_LOGO_HEIGHT}")
+    if not Helper.verify_exist_directory(dst_dir):
+        print(f"【错误】无效的目标文件夹路径：{dst_dir}")
+        exit()
+
+    for src_png_name in os.listdir(src_dir):
+        if not fnmatch.fnmatch(src_png_name, "*.png"):
+            continue
+
+        src_png_path = src_dir.joinpath(src_png_name)
+        src_logo = crop_logo(src_png_path)
+        if (
+            src_logo.width < DESTINATION_LOGO_WIDTH
+            and src_logo.height < DESTINATION_LOGO_HEIGHT
+        ):
+            print(
+                f"【警告】{src_logo.width} x {src_logo.height} 的源 Logo 分辨率较低，建议使用更高分辨率的图片"
+            )
+            print(f"\t图片路径：{src_png_path}")
+        dst_logo_offset_x = 0
+        dst_logo_offset_y = 0
+        dst_logo = None
+        if src_logo.height * 2 < src_logo.width:
+            dst_logo_width = DESTINATION_LOGO_WIDTH
+            dst_logo_height = int(dst_logo_width * src_logo.height / src_logo.width)
+            dst_logo = src_logo.resize((dst_logo_width, dst_logo_height))
+            dst_logo_offset_y = int((DESTINATION_LOGO_HEIGHT - dst_logo_height) / 2)
+        elif src_logo.height * 2 == src_logo.width:
+            dst_logo = src_logo.resize(
+                (DESTINATION_LOGO_WIDTH, DESTINATION_LOGO_HEIGHT)
+            )
+        else:
+            dst_logo_height = DESTINATION_LOGO_HEIGHT
+            dst_logo_width = int(dst_logo_height * src_logo.width / src_logo.height)
+            dst_logo = src_logo.resize((dst_logo_width, dst_logo_height))
+            dst_logo_offset_x = int((DESTINATION_LOGO_WIDTH - dst_logo_width) / 2)
+
+        dst_png = Image.new(
+            "RGBA", (DESTINATION_LOGO_WIDTH, DESTINATION_LOGO_HEIGHT), (0, 0, 0, 0)
+        )
+        dst_png.paste(dst_logo, (dst_logo_offset_x, dst_logo_offset_y))
+        dst_png_path = dst_dir.joinpath(src_png_name)
+        if dst_png_path.exists() and dst_png_path.is_file():
+            dst_png_path.unlink()
+        dst_png.save(dst_png_path)
