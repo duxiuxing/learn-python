9fba4345138683baaa868b29c51d1a9f8a28ab58
duxiuxing <duxiuxing@foxmail.com>
2026-03-11 23:12:12 +0800

    code review

--- python/tool_ra_thumbnails_rename.py
+++ python/tool_ra_thumbnails_rename.py
@@ -15,7 +15,7 @@ from wiiflow_rom import WiiFlow_Rom
 from wiiflow_roms_db import WiiFlow_RomsDB
 
 
-class RA_RenameThumbnails:
+class RA_ThumbnailsRename:
     @staticmethod
     def default_lpl_file_path():
         return LocalConfigs.retroarch_directory().joinpath(
@@ -122,10 +122,10 @@ class RA_RenameThumbnails:
 
     @staticmethod
     def f5_label_to_game_en_title(lpl_file_path: Path, png_folder_path: Path):
-        RA_RenameThumbnails.f1_label_to_rom_file_title(
+        RA_ThumbnailsRename.f1_label_to_rom_file_title(
             lpl_file_path=lpl_file_path, png_folder_path=png_folder_path
         )
-        RA_RenameThumbnails.f3_rom_file_title_to_game_en_title(
+        RA_ThumbnailsRename.f3_rom_file_title_to_game_en_title(
             lpl_file_path=lpl_file_path, png_folder_path=png_folder_path
         )
 
@@ -135,7 +135,7 @@ if __name__ == "__main__":
 
     lpl_file_path = None
     while True:
-        lpl_file_path = RA_RenameThumbnails.default_lpl_file_path()
+        lpl_file_path = RA_ThumbnailsRename.default_lpl_file_path()
         print("\n即将根据 .lpl 文件对缩略图文件进行重命名")
         print(f"默认 .lpl 文件路径：{lpl_file_path}")
         user_input = input("请输入 .lpl 文件路径，使用默认路径请直接按回车 > ")
@@ -147,14 +147,16 @@ if __name__ == "__main__":
         else:
             print(f"【错误】无效的文件路径：{lpl_file_path}")
 
-    thumbnails_dir = RA_RenameThumbnails.default_thumbnails_directory()
+    thumbnails_dir = RA_ThumbnailsRename.default_thumbnails_directory()
     png_folder_path = None
     while True:
-        print("\n以下是预设的文件夹路径：")
-        print(f"1.{thumbnails_dir.joinpath('Named_Boxarts')}")
-        print(f"2.{thumbnails_dir.joinpath('Named_Logos')}")
-        print(f"3.{thumbnails_dir.joinpath('Named_Snaps')}")
-        print(f"4.{thumbnails_dir.joinpath('Named_Titles')}")
+        print(
+            "\n以下是预设的文件夹路径：\n"
+            f"1.{thumbnails_dir.joinpath('Named_Boxarts')}\n"
+            f"2.{thumbnails_dir.joinpath('Named_Logos')}\n"
+            f"3.{thumbnails_dir.joinpath('Named_Snaps')}\n"
+            f"4.{thumbnails_dir.joinpath('Named_Titles')}"
+        )
         user_input = input(
             "请输入预设文件夹的序号或者自定义的文件夹路径（退出请直接按回车）\n> "
         )
@@ -181,33 +183,35 @@ if __name__ == "__main__":
             else:
                 exit()
 
-        print("\n1. label -> 3wonders.png")
-        print("2. 3wonders.png -> label")
-        print("3. 3wonders.png -> Three Wonders.png")
-        print("4. Three Wonders.png -> 3wonders.png")
-        print("5. label -> Three Wonders.png")
-        print("其他输入表示重新设置文件夹路径")
+        print(
+            "\n1. label -> 3wonders.png\n"
+            "2. 3wonders.png -> label\n"
+            "3. 3wonders.png -> Three Wonders.png\n"
+            "4. Three Wonders.png -> 3wonders.png\n"
+            "5. label -> Three Wonders.png\n"
+            "其他输入表示重新设置文件夹路径"
+        )
         user_input = input("请输入操作的序号 > ")
         try:
             number = int(user_input)
             if number == 1:
-                RA_RenameThumbnails.f1_label_to_rom_file_title(
+                RA_ThumbnailsRename.f1_label_to_rom_file_title(
                     lpl_file_path, png_folder_path
                 )
             elif number == 2:
-                RA_RenameThumbnails.f2_rom_file_title_to_label(
+                RA_ThumbnailsRename.f2_rom_file_title_to_label(
                     lpl_file_path, png_folder_path
                 )
             elif number == 3:
-                RA_RenameThumbnails.f3_rom_file_title_to_game_en_title(
+                RA_ThumbnailsRename.f3_rom_file_title_to_game_en_title(
                     lpl_file_path, png_folder_path
                 )
             elif number == 4:
-                RA_RenameThumbnails.f4_game_en_title_to_rom_file_title(
+                RA_ThumbnailsRename.f4_game_en_title_to_rom_file_title(
                     lpl_file_path, png_folder_path
                 )
             elif number == 5:
-                RA_RenameThumbnails.f5_label_to_game_en_title(
+                RA_ThumbnailsRename.f5_label_to_game_en_title(
                     lpl_file_path, png_folder_path
                 )
         except ValueError:
