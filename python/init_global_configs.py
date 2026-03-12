f15897a8a6e47d671c18f2c52280801733d12c03
duxiuxing <duxiuxing@foxmail.com>
2026-03-13 00:38:31 +0800

    config website

--- python/init_global_configs.py
+++ python/init_global_configs.py
@@ -44,6 +44,7 @@ class Init_Global_Configs:
 
         # WiiFlow_Configs
         WiiFlow_Configs._plugin_name = "CPS1"
+        WiiFlow_Configs._website = "https://github.com/R-Sam-1980/cps1"
 
         WiiFlow_PluginsData()
         RomsXML()
