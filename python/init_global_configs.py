8f9343590593f28b93659ae1916c10e7f2dfafbc
duxiuxing <duxiuxing@foxmail.com>
2026-05-06 14:52:21 +0800

    release_date

--- python/init_global_configs.py
+++ python/init_global_configs.py
@@ -34,7 +34,7 @@ class Init_Global_Configs:
         WiiRA_Configs._core_file_name = Path("fbalpha2012_cps3_libretro_wii.dol")
         WiiRA_Configs._core_info_file_name = Path("fbalpha2012_cps3_libretro.info")
         WiiRA_Configs._db_name = Path("Capcom - CP System III.lpl")
-        WiiRA_Configs._release_date = "2025-11-20"
+        WiiRA_Configs._release_date = "20251120"
         WiiRA_Configs._version = "1.22.2"
         WiiRA_Configs._roms_relative_directory = Path("arcade\\fba\\cps3")
 
