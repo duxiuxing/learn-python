e00ac46aadfb23823c17b9c9b123c698b8959968
R-Sam-1980 <duxiuxing@gmail.com>
2026-06-13 19:33:45 +0800

    init_global_configs_test.py

--- python/init_global_configs.py
+++ python/init_global_configs.py
@@ -52,7 +52,7 @@ class Init_Global_Configs:
             # CPS3 的原生分辨率是 384x224，故使用 24=384x448
             'current_resolution_id = "24"'
         ]
-        WiiRA_Configs.wii_remaps_relative_directory = Path(
+        WiiRA_Configs.remaps_relative_directory = Path(
             "remaps\\FB Alpha 2012 CPS-3"
         )
 
@@ -62,45 +62,3 @@ class Init_Global_Configs:
 
         WiiFlow_PluginsData()
         RomsXML()
-
-
-if __name__ == "__main__":
-    Init_Global_Configs()
-
-    print(
-        "LocalConfigs:\n"
-        f"\trepository_directory = {LocalConfigs.repository_directory}\n"
-        f"\texport_to_directory = {LocalConfigs.export_to_directory}\n"
-        f"\timport_from_directory = {LocalConfigs.import_from_directory}\n"
-        f"\tretroarch_directory = {LocalConfigs.retroarch_directory}\n"
-        f"\tseven_zip_exe_path = {LocalConfigs.seven_zip_exe_path}"
-    )
-
-    print(
-        "RA_Config:\n"
-        f"\tlpl_file_name = {RA_Configs.lpl_file_name}\n"
-        f"\troms_win_relative_directory = {RA_Configs.win_roms_relative_directory}\n"
-        f"\troms_wii_relative_directory = {RA_Configs.wii_roms_relative_directory}\n"
-        f"\tandroid_roms_directory = {RA_Configs.android_roms_directory}\n"
-        f"\tipad_roms_directory = {RA_Configs.ipad_roms_directory}\n"
-        f"\tps3_roms_directory = {RA_Configs.ps3_roms_directory}\n"
-        f"\twin_roms_directory = {RA_Configs.win_roms_directory}\n"
-        f"\txbox_roms_directory = {RA_Configs.xbox_roms_directory}"
-    )
-
-    print(
-        "Wii_AppConfigs:\n"
-        f"\tdefault_short_description = {Wii_AppConfigs.default_short_description}"
-    )
-
-    print(
-        "WiiRA_Configs:\n"
-        f"\tcore_file_name = {WiiRA_Configs.core_file_name}\n"
-        f"\tcore_info_file_name = {WiiRA_Configs.core_info_file_name}\n"
-        f"\tcore_name = {WiiRA_Configs.core_name}\n"
-        f"\trelease_date = {WiiRA_Configs.release_date}\n"
-        f"\tversion = {WiiRA_Configs.version}\n"
-        f"\twii_remaps_relative_directory = {WiiRA_Configs.wii_remaps_relative_directory}"
-    )
-
-    print("WiiFlow_Configs:\n" f"\tplugin_name = {WiiFlow_Configs.plugin_name}")
