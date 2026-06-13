697a86025e5016e76e82b7c315fc202a64f7d78b
R-Sam-1980 <duxiuxing@gmail.com>
2026-06-14 03:38:40 +0800

    update CPS3 Apps (RA核心，SD卡版).md

--- python/cps3_doc_wii_ra_apps.py
+++ python/cps3_doc_wii_ra_apps.py
@@ -7,7 +7,9 @@ from helper import Helper
 from init_global_configs import Init_Global_Configs
 from local_configs import LocalConfigs
 from pathlib import Path
+from wii_app_configs import Wii_AppConfigs
 from wii_app_info import Wii_AppInfo
+from wii_ra_configs import WiiRA_Configs
 from wiiflow_configs import WiiFlow_Configs
 from wiiflow_game import WiiFlow_Game
 from wiiflow_games_db import WiiFlow_GamesDB
@@ -19,34 +21,34 @@ from wiiflow_roms_db import WiiFlow_RomsDB
 if __name__ == "__main__":
     Init_Global_Configs()
 
-    app_list = []
+    app_info_list = []
     apps_dir = LocalConfigs.export_to_directory.joinpath("apps")
     for app_folder_name in os.listdir(apps_dir):
         meta_xml_path = apps_dir.joinpath(f"{app_folder_name}\\meta.xml")
         if not meta_xml_path.exists() or not meta_xml_path.is_file():
             continue
 
-        app = Wii_AppInfo(app_folder_name)
+        app_info = Wii_AppInfo(app_folder_name)
         tree = ET.parse(meta_xml_path)
         root_elem = tree.getroot()
         for elem in root_elem:
             if elem.tag == "name":
-                app.name = elem.text
+                app_info.name = elem.text
             elif elem.tag == "arguments":
                 for arg_elem in elem.findall("arg"):
                     if WiiFlow_Configs.is_rom_file_name(arg_elem.text):
-                        app.rom_file_name = arg_elem.text
+                        app_info.rom_file_name = arg_elem.text
                         rom = WiiFlow_RomsDB.query_rom(
                             rom_file_title=Path(arg_elem.text).stem
                         )
                         game = WiiFlow_GamesDB.query_game(game_id=rom.game_id)
-                        app.game_zhcn_title = game.zhcn_title
+                        app_info.game_zhcn_title = game.zhcn_title
 
-        if app.rom_file_name is not None:
-            app_list.append(app)
+        if app_info.rom_file_name is not None:
+            app_info_list.append(app_info)
 
     doc_path = LocalConfigs.export_to_directory.joinpath(
-        "CPS3 Apps (RetroArch).md",
+        "CPS3 Apps (RA核心，SD卡版).md",
     )
     if not Helper.verify_exist_directory_ex(doc_path.parent):
         print(f"【错误】无效的目标文件 {doc_path}")
@@ -57,18 +59,18 @@ if __name__ == "__main__":
     with open(doc_path, "w", encoding="utf-8") as doc:
         doc.write(
             "# CPS3 街机游戏 App 列表\n\n"
-            "1G1R1App 是 one Game one ROM one App 的缩写，意思是一个游戏只选取一个版本的 ROM 文件，同时还有一个独立的 App 专门负责加载这个游戏的 ROM 文件。\n\n"
-            "以下是基于 Wii 版 RetroArch 的 fbalpha2012_cps1_libretro_wii.dol 核心制作的，CPS3 街机游戏 App 列表：\n\n"
+            "1G1R1A 是 one Game one ROM one App 的缩写，意思是一个游戏只选取一个版本的 ROM 文件，同时还有一个独立的 App 专门负责加载这个游戏的 ROM 文件。\n\n"
+            "以下这些 CPS3 街机游戏 App，基于 Wii 版 RetroArch 的 CPS3 核心（fbalpha2012_cps3_libretro_wii.dol）制作，必须和游戏 ROM 文件一起放置在 SD 卡才能正常运行：\n\n"
             "## 按 App 名称排序\n\n"
             "序号 | App 名称 | App 图标 | 游戏中文名 | ROM 文件\n"
             "--- | --- | --- | --- | ---\n"
         )
 
         index = 0
-        for app in sorted(app_list, key=lambda x: x.name):
+        for app_info in sorted(app_info_list, key=lambda x: x.name):
             index = index + 1
             doc.write(
-                f"{index} | {app.name} | ![](./apps/{app.folder_name}/icon.png) | {app.game_zhcn_title[4:]} | {app.rom_file_name}\n"
+                f"{index} | {app_info.name} | ![](./apps/{app_info.folder_name}/icon.png) | {app_info.game_zhcn_title[4:]} | {app_info.rom_file_name}\n"
             )
 
         doc.write(
@@ -78,9 +80,9 @@ if __name__ == "__main__":
         )
 
         index = 0
-        for app in sorted(app_list, key=lambda x: x.game_zhcn_title):
+        for app_info in sorted(app_info_list, key=lambda x: x.game_zhcn_title):
             index = index + 1
             doc.write(
-                f"{index} | {app.game_zhcn_title} | ![](./apps/{app.folder_name}/icon.png) | {app.name} | {app.rom_file_name}\n"
+                f"{index} | {app_info.game_zhcn_title} | ![](./apps/{app_info.folder_name}/icon.png) | {app_info.name} | {app_info.rom_file_name}\n"
             )
         doc.close()
--- python/cps3_wii_apps.py
+++ python/cps3_wii_apps.py
@@ -13,17 +13,6 @@ from wii_ra_app import WiiRA_App
 if __name__ == "__main__":
     Init_Global_Configs()
 
-    cps3_ra_app_configs = Wii_AppConfigs(
-        app_name="Capcom - CP System III",
-        base_app_folder_name="cps3",
-        rom=None,
-    )
-    cps3_ra_app_configs.long_description = (
-        "- Emulator for CPS-3 games based on RetroArch\n"
-        "- Based on a snapshot of the FB Alpha codebase from 2012\n"
-        "- Compatible with FB Alpha v0.2.97.29 ROM sets"
-    )
-
     add_game_app_configs(rom_file_title="jojo", remap="jojo")
     add_game_app_configs(
         rom_file_title="jojoba", app_name="JoJo's Venture 2", remap="jojo"
@@ -32,6 +21,17 @@ if __name__ == "__main__":
     add_game_app_configs(rom_file_title="sfiii", app_name="Street Fighter 3.1")
     add_game_app_configs(rom_file_title="sfiii2", app_name="Street Fighter 3.2")
     add_game_app_configs(rom_file_title="sfiii3", app_name="Street Fighter 3.3")
+
+    wiiflow_plugin_ra_app_configs = Wii_AppConfigs(
+        app_name="Capcom - CP System III",
+        base_app_folder_name="cps3",
+        rom=None,
+    )
+    wiiflow_plugin_ra_app_configs.long_description = (
+        "- Emulator for CPS-3 games based on RetroArch\n"
+        "- Based on a snapshot of the FB Alpha codebase from 2012\n"
+        "- Compatible with FB Alpha v0.2.97.29 ROM sets"
+    )
 
     while True:
         export_to_dir = LocalConfigs.export_to_directory
@@ -48,7 +48,8 @@ if __name__ == "__main__":
 
         print(
             "\n1. 导出 ROM 文件\n"
-            "2. 导出 App (retroarch-wii 核心) 到 wii-ra-apps-sd\n"
+            "2. 导出 Game App (RA 核心) 到 wii-ra-apps-sd\n"
+            "3. 导出 Wiiflow Plugin App (RA 核心) 到 wiiflow-sd\n"
             "其他输入表示退出"
         )
         user_input = input("请输入操作的序号 > ")
@@ -56,24 +57,26 @@ if __name__ == "__main__":
         try:
             number = int(user_input)
             if number == 1:
-                cps3_ra_app_configs.init_playlist_configs()
-                RA_Playlist(cps3_ra_app_configs.playlist_configs).export_rom_files()
+                wiiflow_plugin_ra_app_configs.init_playlist_configs()
+                RA_Playlist(
+                    wiiflow_plugin_ra_app_configs.playlist_configs
+                ).export_rom_files()
             elif number == 2:
-                device = Wii_AppConfigs.DEVICE_SD
-
-                cps3_ra_app_configs.device = device
-                cps3_ra_app_configs.init_playlist_configs()
-                cps3_ra_app_configs.playlist_configs.boxarts_folder = None
-                cps3_ra_app_configs.playlist_configs.logos_folder = None
-                cps3_ra_app_configs.playlist_configs.snaps_folder = None
-                cps3_ra_app_configs.playlist_configs.titles_folder = None
-                cps3_ra_app_configs.use_favorites_as_playlist = True
-                WiiRA_App(cps3_ra_app_configs).export_all()
-
+                # Game App (RA 核心)
                 for game_app_configs in game_app_configs_list:
-                    game_app_configs.device = device
+                    game_app_configs.device = Wii_AppConfigs.DEVICE_SD
                     game_app = WiiRA_App(game_app_configs)
                     game_app.export_all()
+            elif number == 3:
+                # Wiiflow Plugin App (RA 核心)
+                wiiflow_plugin_ra_app_configs.device = Wii_AppConfigs.DEVICE_SD
+                wiiflow_plugin_ra_app_configs.init_playlist_configs()
+                wiiflow_plugin_ra_app_configs.playlist_configs.boxarts_folder = None
+                wiiflow_plugin_ra_app_configs.playlist_configs.logos_folder = None
+                wiiflow_plugin_ra_app_configs.playlist_configs.snaps_folder = None
+                wiiflow_plugin_ra_app_configs.playlist_configs.titles_folder = None
+                wiiflow_plugin_ra_app_configs.use_favorites_as_playlist = True
+                WiiRA_App(wiiflow_plugin_ra_app_configs).export_all()
             else:
                 break
         except ValueError:
