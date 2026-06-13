2fe5b1f1fe6b76f6f927fe1dc15806cc12a2d0b1
R-Sam-1980 <duxiuxing@gmail.com>
2026-06-14 05:40:04 +0800

    update CPS2 Apps (RA核心1.22.2版).md

--- python/cps2_wii_apps.py
+++ python/cps2_wii_apps.py
@@ -14,30 +14,6 @@ from wii_ss_app import WiiSS_App
 if __name__ == "__main__":
     Init_Global_Configs()
 
-    cps2_ra_app_configs = Wii_AppConfigs(
-        app_name="Capcom - CP System II",
-        base_app_folder_name="cps2",
-        rom=None,
-    )
-    cps2_ra_app_configs.long_description = (
-        "- Emulator for CPS-2 games based on RetroArch\n"
-        "- Based on a snapshot of the FB Alpha codebase from 2012\n"
-        "- Compatible with FB Alpha v0.2.97.29 ROM sets"
-    )
-
-    # 基于 RA-HEXAECO 核心的 App
-    cps2_ss_app_configs = Wii_AppConfigs(
-        app_name="RA-SS CPS-2",
-        base_app_folder_name="cps2",
-        rom=None,
-    )
-    cps2_ss_app_configs.long_description = (
-        "- Mod By RunningSnakes\n"
-        "- Emulator for CPS-2 games based on RA-SS Hexaeco\n"
-        "- Based on a snapshot of the FB Alpha codebase from 2012\n"
-        "- Compatible with FB Alpha v0.2.97.29 ROM sets"
-    )
-
     add_game_app_configs(rom_file_title="1944")
     add_game_app_configs(rom_file_title="19xx", app_name="19XX")
     add_game_app_configs(rom_file_title="armwar")
@@ -80,6 +56,29 @@ if __name__ == "__main__":
     add_game_app_configs(rom_file_title="xmcota", app_name="X-Men")
     add_game_app_configs(rom_file_title="xmvsf")
 
+    wiiflow_plugin_ra_app_configs = Wii_AppConfigs(
+        app_name="Capcom - CP System II",
+        base_app_folder_name="cps2",
+        rom=None,
+    )
+    wiiflow_plugin_ra_app_configs.long_description = (
+        "- Emulator for CPS-2 games based on RetroArch\n"
+        "- Based on a snapshot of the FB Alpha codebase from 2012\n"
+        "- Compatible with FB Alpha v0.2.97.29 ROM sets"
+    )
+
+    wiiflow_plugin_ss_app_configs = Wii_AppConfigs(
+        app_name="RA-SS CPS-2",
+        base_app_folder_name="cps2",
+        rom=None,
+    )
+    wiiflow_plugin_ss_app_configs.long_description = (
+        "- Mod By RunningSnakes\n"
+        "- Emulator for CPS-2 games based on RA-SS Hexaeco\n"
+        "- Based on a snapshot of the FB Alpha codebase from 2012\n"
+        "- Compatible with FB Alpha v0.2.97.29 ROM sets"
+    )
+
     while True:
         export_to_dir = LocalConfigs.export_to_directory
         print(f"\n即将导出 Wii App 到目标文件夹\n默认目标文件夹路径：{export_to_dir}")
@@ -95,9 +94,11 @@ if __name__ == "__main__":
 
         print(
             "\n1. 导出 ROM 文件\n"
-            "2. 导出 App (retroarch-wii 核心) 到 wii-ra-apps-sd\n"
-            "3. 导出 App (RA-HEXAECO 核心) 到 wii-ss-apps-sd\n"
-            "4. 导出 App (RA-HEXAECO 核心) 到 wii-ss-apps-usb\n"
+            "2. 导出 Game App (RA 核心) 到 wii-ra-apps-sd\n"
+            "3. 导出 Wiiflow Plugin App (RA 核心) 到 wiiflow-sd\n"
+            "4. 导出 Game App (SS 核心) 到 wii-ss-apps-sd\n"
+            "5. 导出 Wiiflow Plugin App (SS 核心) 到 wiiflow-sd\n"
+            "6. 导出 Game App (SS 核心) 到 wii-ss-apps-usb\n"
             "其他输入表示退出"
         )
         user_input = input("请输入操作的序号 > ")
@@ -105,42 +106,40 @@ if __name__ == "__main__":
         try:
             number = int(user_input)
             if number == 1:
-                cps2_ra_app_configs.init_playlist_configs()
-                RA_Playlist(cps2_ra_app_configs.playlist_configs).export_rom_files()
+                wiiflow_plugin_ra_app_configs.init_playlist_configs()
+                RA_Playlist(
+                    wiiflow_plugin_ra_app_configs.playlist_configs
+                ).export_rom_files()
             elif number == 2:
-                device = Wii_AppConfigs.DEVICE_SD
-
-                cps2_ra_app_configs.device = device
-                cps2_ra_app_configs.init_playlist_configs()
-                cps2_ra_app_configs.playlist_configs.boxarts_folder = None
-                cps2_ra_app_configs.playlist_configs.logos_folder = None
-                cps2_ra_app_configs.playlist_configs.snaps_folder = None
-                cps2_ra_app_configs.playlist_configs.titles_folder = None
-                cps2_ra_app_configs.use_favorites_as_playlist = False
-                WiiRA_App(cps2_ra_app_configs).export_all()
-
+                # Game App (RA 核心， SD 版)
                 for game_app_configs in game_app_configs_list:
-                    game_app_configs.device = device
+                    game_app_configs.device = Wii_AppConfigs.DEVICE_SD
                     game_app = WiiRA_App(game_app_configs)
                     game_app.export_all()
             elif number == 3:
-                device = Wii_AppConfigs.DEVICE_SD
-
-                cps2_ss_app_configs.device = device
-                WiiSS_App(cps2_ss_app_configs).export_all()
-
+                # Wiiflow Plugin App (RA 核心， SD 版)
+                wiiflow_plugin_ra_app_configs.device = Wii_AppConfigs.DEVICE_SD
+                wiiflow_plugin_ra_app_configs.init_playlist_configs()
+                wiiflow_plugin_ra_app_configs.playlist_configs.boxarts_folder = None
+                wiiflow_plugin_ra_app_configs.playlist_configs.logos_folder = None
+                wiiflow_plugin_ra_app_configs.playlist_configs.snaps_folder = None
+                wiiflow_plugin_ra_app_configs.playlist_configs.titles_folder = None
+                wiiflow_plugin_ra_app_configs.use_favorites_as_playlist = True
+                WiiRA_App(wiiflow_plugin_ra_app_configs).export_all()
+            elif number == 4:
+                # Game App (SS 核心， SD 版)
                 for game_app_configs in game_app_configs_list:
-                    game_app_configs.device = device
+                    game_app_configs.device = Wii_AppConfigs.DEVICE_SD
                     game_app = WiiSS_App(game_app_configs)
                     game_app.export_all()
-            elif number == 4:
-                device = Wii_AppConfigs.DEVICE_USB
-
-                cps2_ss_app_configs.device = device
-                WiiSS_App(cps2_ss_app_configs).export_all()
-
+            elif number == 5:
+                # Wiiflow Plugin App (SS 核心， SD 版)
+                wiiflow_plugin_ss_app_configs.device = Wii_AppConfigs.DEVICE_SD
+                WiiSS_App(wiiflow_plugin_ss_app_configs).export_all()
+            elif number == 6:
+                # Game App (SS 核心， USB 版)
                 for game_app_configs in game_app_configs_list:
-                    game_app_configs.device = device
+                    game_app_configs.device = Wii_AppConfigs.DEVICE_USB
                     game_app = WiiSS_App(game_app_configs)
                     game_app.export_all()
             else:
