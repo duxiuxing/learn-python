3533ce0429b98228043450f1585c3b0cf807e5a8
R-Sam-1980 <duxiuxing@gmail.com>
2026-06-14 16:25:02 +0800

    update CPS1 Apps (SS核心)

--- python/init_global_configs.py
+++ python/init_global_configs.py
@@ -58,6 +58,13 @@ class Init_Global_Configs:
         WiiSS_Configs.core_file_name = Path("Arcade CPS1.dol")
         WiiSS_Configs.data_folder_name = "C1MOD"
         WiiSS_Configs.release_date = "20220508"
+        WiiSS_Configs.settings_list = [
+            # 宽高比：0=4:3
+            'aspect_ratio_index = "0"',
+            'aspect_ratio_index_wide = "0"',
+            # CPS1 的原生分辨率是 384x224，故使用 23=384x448
+            'video_vres = "23"',
+        ]
 
         # WiiFlow_Configs
         WiiFlow_Configs.plugin_name = "CPS1"
