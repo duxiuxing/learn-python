e196c6351e1dc1ee97cbc21d4e4234be842d51c7
R-Sam-1980 <duxiuxing@gmail.com>
2026-06-16 09:22:41 +0800

    update CPS2 Apps (SS核心)

--- python/cps2_wii_apps.py
+++ python/cps2_wii_apps.py
@@ -5,74 +5,111 @@ from local_configs import LocalConfigs
 from pathlib import Path
 from ra_playlist import RA_Playlist
 from ra_playlist_configs import RA_PlaylistConfigs
-from wii_app_configs import add_game_app_configs
-from wii_app_configs import game_app_configs_list
 from wii_app_configs import Wii_AppConfigs
+from wii_app_factory import Wii_AppFactory
 from wii_ra_app import WiiRA_App
 from wii_ss_app import WiiSS_App
 
 if __name__ == "__main__":
     Init_Global_Configs()
 
-    add_game_app_configs(rom_file_title="1944")
-    add_game_app_configs(rom_file_title="19xx", app_name="19XX")
-    add_game_app_configs(rom_file_title="armwar")
-    add_game_app_configs(rom_file_title="avsp")
-    add_game_app_configs(rom_file_title="batcir")
-    add_game_app_configs(rom_file_title="choko")
-    add_game_app_configs(rom_file_title="csclub")
-    add_game_app_configs(rom_file_title="cybots", app_name="Cyberbots")
-    add_game_app_configs(rom_file_title="ddsom", app_name="Dungeons & Dragons 2")
-    add_game_app_configs(rom_file_title="ddtod", app_name="Dungeons & Dragons")
-    add_game_app_configs(rom_file_title="dimahoo")
-    add_game_app_configs(rom_file_title="dstlk", app_name="Darkstalkers")
-    add_game_app_configs(rom_file_title="ecofghtr")
-    add_game_app_configs(rom_file_title="gigawing")
-    add_game_app_configs(rom_file_title="hsf2", app_name="Hyper Street Fighter 2")
-    add_game_app_configs(rom_file_title="jyangoku", app_name="Jyangokushi")
-    add_game_app_configs(rom_file_title="megaman2", app_name="Mega Man 2")
-    # add_game_app_configs(rom_file_title="mmancp2u")
-    add_game_app_configs(rom_file_title="mmatrix", app_name="Mars Matrix")
-    add_game_app_configs(rom_file_title="mpang")
-    add_game_app_configs(rom_file_title="msh")
-    add_game_app_configs(rom_file_title="mshvsf", app_name="Marvel vs. Street Fighter")
-    add_game_app_configs(rom_file_title="mvsc", app_name="Marvel vs. Capcom")
-    add_game_app_configs(rom_file_title="nwarr", app_name="Vampire Hunter")
-    add_game_app_configs(rom_file_title="progear")
-    add_game_app_configs(rom_file_title="pzloop2")
-    add_game_app_configs(rom_file_title="qndream", app_name="Quiz Nanairo Dreams")
-    add_game_app_configs(rom_file_title="ringdest", app_name="Slam Masters 2")
-    add_game_app_configs(rom_file_title="sfa", app_name="Street Fighter Alpha")
-    add_game_app_configs(rom_file_title="sfa2")
-    add_game_app_configs(rom_file_title="sfa3")
-    add_game_app_configs(rom_file_title="sfz2al")
-    add_game_app_configs(rom_file_title="sgemf", app_name="Super Gem Fighter")
-    add_game_app_configs(rom_file_title="spf2t", app_name="Super Puzzle Fighter")
-    add_game_app_configs(rom_file_title="ssf2", app_name="Super Street Fighter 2")
-    add_game_app_configs(rom_file_title="ssf2t", app_name="Super Street Fighter 2X")
-    add_game_app_configs(rom_file_title="vhunt2", app_name="Vampire Hunter 2")
-    add_game_app_configs(rom_file_title="vsav", app_name="Vampire Savior")
-    add_game_app_configs(rom_file_title="vsav2", app_name="Vampire Savior 2")
-    add_game_app_configs(rom_file_title="xmcota", app_name="X-Men")
-    add_game_app_configs(rom_file_title="xmvsf")
+    Wii_AppFactory.add_game_app_configs(rom_file_title="1944")
+    Wii_AppFactory.add_game_app_configs(rom_file_title="19xx", app_name="19XX")
+    Wii_AppFactory.add_game_app_configs(rom_file_title="armwar")
+    Wii_AppFactory.add_game_app_configs(rom_file_title="avsp")
+    Wii_AppFactory.add_game_app_configs(rom_file_title="batcir")
+    Wii_AppFactory.add_game_app_configs(rom_file_title="choko")
+    Wii_AppFactory.add_game_app_configs(rom_file_title="csclub")
+    Wii_AppFactory.add_game_app_configs(rom_file_title="cybots", app_name="Cyberbots")
+    Wii_AppFactory.add_game_app_configs(
+        rom_file_title="ddsom", app_name="Dungeons & Dragons 2"
+    )
+    Wii_AppFactory.add_game_app_configs(
+        rom_file_title="ddtod", app_name="Dungeons & Dragons"
+    )
+    Wii_AppFactory.add_game_app_configs(rom_file_title="dimahoo")
+    Wii_AppFactory.add_game_app_configs(rom_file_title="dstlk", app_name="Darkstalkers")
+    Wii_AppFactory.add_game_app_configs(rom_file_title="ecofghtr")
+    Wii_AppFactory.add_game_app_configs(rom_file_title="gigawing")
+    Wii_AppFactory.add_game_app_configs(
+        rom_file_title="hsf2", app_name="Hyper Street Fighter 2"
+    )
+    Wii_AppFactory.add_game_app_configs(
+        rom_file_title="jyangoku", app_name="Jyangokushi"
+    )
+    Wii_AppFactory.add_game_app_configs(
+        rom_file_title="megaman2", app_name="Mega Man 2"
+    )
+    # Wii_AppFactory.add_game_app_configs(rom_file_title="mmancp2u")
+    Wii_AppFactory.add_game_app_configs(
+        rom_file_title="mmatrix", app_name="Mars Matrix"
+    )
+    Wii_AppFactory.add_game_app_configs(rom_file_title="mpang")
+    Wii_AppFactory.add_game_app_configs(rom_file_title="msh")
+    Wii_AppFactory.add_game_app_configs(
+        rom_file_title="mshvsf", app_name="Marvel vs. Street Fighter"
+    )
+    Wii_AppFactory.add_game_app_configs(
+        rom_file_title="mvsc", app_name="Marvel vs. Capcom"
+    )
+    Wii_AppFactory.add_game_app_configs(
+        rom_file_title="nwarr", app_name="Vampire Hunter"
+    )
+    Wii_AppFactory.add_game_app_configs(rom_file_title="progear")
+    Wii_AppFactory.add_game_app_configs(rom_file_title="pzloop2")
+    Wii_AppFactory.add_game_app_configs(
+        rom_file_title="qndream", app_name="Quiz Nanairo Dreams"
+    )
+    Wii_AppFactory.add_game_app_configs(
+        rom_file_title="ringdest", app_name="Slam Masters 2"
+    )
+    Wii_AppFactory.add_game_app_configs(
+        rom_file_title="sfa", app_name="Street Fighter Alpha"
+    )
+    Wii_AppFactory.add_game_app_configs(rom_file_title="sfa2")
+    Wii_AppFactory.add_game_app_configs(rom_file_title="sfa3")
+    Wii_AppFactory.add_game_app_configs(rom_file_title="sfz2al")
+    Wii_AppFactory.add_game_app_configs(
+        rom_file_title="sgemf", app_name="Super Gem Fighter"
+    )
+    Wii_AppFactory.add_game_app_configs(
+        rom_file_title="spf2t", app_name="Super Puzzle Fighter"
+    )
+    Wii_AppFactory.add_game_app_configs(
+        rom_file_title="ssf2", app_name="Super Street Fighter 2"
+    )
+    Wii_AppFactory.add_game_app_configs(
+        rom_file_title="ssf2t", app_name="Super Street Fighter 2X"
+    )
+    Wii_AppFactory.add_game_app_configs(
+        rom_file_title="vhunt2", app_name="Vampire Hunter 2"
+    )
+    Wii_AppFactory.add_game_app_configs(
+        rom_file_title="vsav", app_name="Vampire Savior"
+    )
+    Wii_AppFactory.add_game_app_configs(
+        rom_file_title="vsav2", app_name="Vampire Savior 2"
+    )
+    Wii_AppFactory.add_game_app_configs(rom_file_title="xmcota", app_name="X-Men")
+    Wii_AppFactory.add_game_app_configs(rom_file_title="xmvsf")
 
-    wiiflow_plugin_ra_app_configs = Wii_AppConfigs(
+    plugin_ra_app_configs = Wii_AppConfigs(
         app_name="Capcom - CP System II",
-        base_app_folder_name="cps2",
+        app_folder_base_name="cps2",
         rom=None,
     )
-    wiiflow_plugin_ra_app_configs.long_description = (
+    plugin_ra_app_configs.long_description = (
         "- Emulator for CPS-2 games based on RetroArch\n"
         "- Based on a snapshot of the FB Alpha codebase from 2012\n"
         "- Compatible with FB Alpha v0.2.97.29 ROM sets"
     )
 
-    wiiflow_plugin_ss_app_configs = Wii_AppConfigs(
+    plugin_ss_app_configs = Wii_AppConfigs(
         app_name="RA-SS CPS-2",
-        base_app_folder_name="cps2",
+        app_folder_base_name="cps2",
         rom=None,
     )
-    wiiflow_plugin_ss_app_configs.long_description = (
+    plugin_ss_app_configs.long_description = (
         "- Mod By RunningSnakes based on RA-SS Hexaeco\n"
         "- Emulator for CPS-2 games\n"
         "- Based on a snapshot of the FB Alpha codebase from 2012\n"
@@ -106,42 +143,33 @@ if __name__ == "__main__":
         try:
             number = int(user_input)
             if number == 1:
-                wiiflow_plugin_ra_app_configs.init_playlist_configs()
-                RA_Playlist(
-                    wiiflow_plugin_ra_app_configs.playlist_configs
-                ).export_rom_files()
+                # 导出 ROM 文件
+                RA_Playlist(WiiRA_App.init_playlist_configs()).export_rom_files()
             elif number == 2:
                 # Game App (RA 核心， SD 版)
-                for game_app_configs in game_app_configs_list:
-                    game_app_configs.device = Wii_AppConfigs.DEVICE_SD
-                    game_app = WiiRA_App(game_app_configs)
-                    game_app.export_all()
+                Wii_AppFactory.export_game_ra_apps(Wii_AppConfigs.DEVICE_SD)
             elif number == 3:
                 # Wiiflow Plugin App (RA 核心， SD 版)
-                wiiflow_plugin_ra_app_configs.device = Wii_AppConfigs.DEVICE_SD
-                wiiflow_plugin_ra_app_configs.init_playlist_configs()
-                wiiflow_plugin_ra_app_configs.playlist_configs.boxarts_folder = None
-                wiiflow_plugin_ra_app_configs.playlist_configs.logos_folder = None
-                wiiflow_plugin_ra_app_configs.playlist_configs.snaps_folder = None
-                wiiflow_plugin_ra_app_configs.playlist_configs.titles_folder = None
-                wiiflow_plugin_ra_app_configs.use_favorites_as_playlist = True
-                WiiRA_App(wiiflow_plugin_ra_app_configs).export_all()
+                plugin_ra_app_configs.device = Wii_AppConfigs.DEVICE_SD
+                plugin_ra_app_configs.use_favorites_as_playlist = False
+
+                playlist_configs = WiiRA_App.init_playlist_configs()
+                playlist_configs.boxarts_folder = None
+                playlist_configs.logos_folder = None
+                playlist_configs.snaps_folder = None
+                playlist_configs.titles_folder = None
+
+                WiiRA_App(plugin_ra_app_configs, playlist_configs).export_all()
             elif number == 4:
                 # Game App (SS 核心， SD 版)
-                for game_app_configs in game_app_configs_list:
-                    game_app_configs.device = Wii_AppConfigs.DEVICE_SD
-                    game_app = WiiSS_App(game_app_configs)
-                    game_app.export_all()
+                Wii_AppFactory.export_game_ss_apps(Wii_AppConfigs.DEVICE_SD)
             elif number == 5:
                 # Wiiflow Plugin App (SS 核心， SD 版)
-                wiiflow_plugin_ss_app_configs.device = Wii_AppConfigs.DEVICE_SD
-                WiiSS_App(wiiflow_plugin_ss_app_configs).export_all()
+                plugin_ss_app_configs.device = Wii_AppConfigs.DEVICE_SD
+                WiiSS_App(plugin_ss_app_configs).export_all()
             elif number == 6:
                 # Game App (SS 核心， USB 版)
-                for game_app_configs in game_app_configs_list:
-                    game_app_configs.device = Wii_AppConfigs.DEVICE_USB
-                    game_app = WiiSS_App(game_app_configs)
-                    game_app.export_all()
+                Wii_AppFactory.export_game_ss_apps(Wii_AppConfigs.DEVICE_USB)
             else:
                 break
         except ValueError:
