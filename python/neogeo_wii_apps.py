5bd58dc2a27f6b392a677178908984e124fda6c8
R-Sam-1980 <duxiuxing@gmail.com>
2026-06-17 07:11:20 +0800

    code review

--- python/neogeo_wii_apps.py
+++ python/neogeo_wii_apps.py
@@ -81,6 +81,7 @@ if __name__ == "__main__":
         app_folder_base_name="neogeo",
         rom=None,
     )
+    plugin_ss_app_configs.cfg_file_name = "main.cfg"
     plugin_ss_app_configs.long_description = (
         "- Mod By RunningSnakes based on RA-SS Hexaeco\n"
         "- Emulator for Neo Geo games smaller than 23 MB\n"
@@ -123,7 +124,6 @@ if __name__ == "__main__":
             elif number == 3:
                 # Wiiflow Plugin App (RA 核心， SD 版)
                 plugin_ra_app_configs.device = Wii_AppConfigs.DEVICE_SD
-                plugin_ra_app_configs.use_favorites_as_playlist = False
 
                 playlist_configs = WiiRA_App.init_playlist_configs()
                 playlist_configs.boxarts_folder = None
