51a6277383648129822797a9f139b29633e39431
R-Sam-1980 <duxiuxing@gmail.com>
2026-07-10 07:45:59 +0800

    wii channel : sf2 and sf2hf

--- python/cps1_wii_make_channel_png.py
+++ python/cps1_wii_make_channel_png.py
@@ -56,6 +56,32 @@ if __name__ == "__main__":
     )
     wide_banner_configs_list.append(wide_banner_configs)
 
+    rom_file_title = "sf2"
+    rom_file_title_list.append(rom_file_title)
+    standard_banner_configs = StandardBannerConfigs(
+        rom_file_title,
+        game_logo_size=(340, 170),
+        game_logo_left_top=(240, 80),
+        company_logo_left_top=StandardBannerConfigs.COMPANY_LOGO_ALIGN_RIGHT_BOTTOM,
+        index=1,
+    )
+    standard_banner_configs_list.append(standard_banner_configs)
+    standard_banner_configs = StandardBannerConfigs(
+        rom_file_title,
+        game_logo_size=(220, 110),
+        game_logo_left_top=(350, 110),
+        company_logo_left_top=StandardBannerConfigs.COMPANY_LOGO_ALIGN_LEFT_BOTTOM,
+        index=2,
+    )
+    standard_banner_configs_list.append(standard_banner_configs)
+    wide_banner_configs = WideBannerConfigs(
+        rom_file_title,
+        game_logo_size=(400, 200),
+        game_logo_left_top=(60, 80),
+        company_logo_left_top=WideBannerConfigs.COMPANY_LOGO_ALIGN_LEFT_TOP,
+    )
+    wide_banner_configs_list.append(wide_banner_configs)
+
     rom_file_title = "sf2ce"
     rom_file_title_list.append(rom_file_title)
     standard_banner_configs = StandardBannerConfigs(
@@ -68,34 +94,59 @@ if __name__ == "__main__":
     wide_banner_configs = WideBannerConfigs(
         rom_file_title,
         game_logo_size=(400, 200),
-        game_logo_left_top=(35, 80),
+        game_logo_left_top=(60, 80),
         company_logo_left_top=WideBannerConfigs.COMPANY_LOGO_ALIGN_LEFT_TOP,
     )
     wide_banner_configs_list.append(wide_banner_configs)
 
-    rom_file_title_filter = rom_file_title
+    rom_file_title = "sf2hf"
+    rom_file_title_list.append(rom_file_title)
+    standard_banner_configs = StandardBannerConfigs(
+        rom_file_title,
+        game_logo_size=(340, 170),
+        game_logo_left_top=(240, 80),
+        company_logo_left_top=StandardBannerConfigs.COMPANY_LOGO_ALIGN_RIGHT_BOTTOM,
+        index=1,
+    )
+    standard_banner_configs_list.append(standard_banner_configs)
+    standard_banner_configs = StandardBannerConfigs(
+        rom_file_title,
+        game_logo_size=(160, 80),
+        game_logo_left_top=(400, StandardBannerConfigs.COMPANY_LOGO_OFFSET_Y),
+        company_logo_left_top=(23, 16),
+        index=2,
+    )
+    standard_banner_configs_list.append(standard_banner_configs)
+    standard_banner_configs = StandardBannerConfigs(
+        rom_file_title,
+        game_logo_size=(170, 85),
+        game_logo_left_top=(8, StandardBannerConfigs.COMPANY_LOGO_OFFSET_Y),
+        company_logo_left_top=StandardBannerConfigs.COMPANY_LOGO_ALIGN_RIGHT_BOTTOM,
+        index=3,
+    )
+    standard_banner_configs_list.append(standard_banner_configs)
+    wide_banner_configs = WideBannerConfigs(
+        rom_file_title,
+        game_logo_size=(400, 200),
+        game_logo_left_top=(60, 80),
+        company_logo_left_top=WideBannerConfigs.COMPANY_LOGO_ALIGN_LEFT_TOP,
+    )
+    wide_banner_configs_list.append(wide_banner_configs)
 
     for title in rom_file_title_list:
         if rom_file_title_filter is None:
             WiiChannel_Icon(title).make()
         elif rom_file_title_filter == title:
             WiiChannel_Icon(title).make()
-            break
 
     for configs in standard_banner_configs_list:
         if rom_file_title_filter is None:
             StandardBanner(configs).make()
         elif rom_file_title_filter == configs.rom_file_title:
             StandardBanner(configs).make()
-            break
 
     for configs in wide_banner_configs_list:
         if rom_file_title_filter is None:
             WideBanner(configs).make()
         elif rom_file_title_filter == configs.rom_file_title:
             WideBanner(configs).make()
-            break
-
-    # WiiChannel_Icon(rom_file_title).make()
-    # StandardBanner(standard_banner_configs).make()
-    # WideBanner(wide_banner_configs).make()
